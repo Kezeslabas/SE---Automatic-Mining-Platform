8361d36445706c62f354ce890ab42ecdbe8d6c0f
Kezeslabas <cari.gergo@gmail.com>
2021-02-06 08:47:54 +0100

    added generic startpoint file

---
+++ gamma.cs
@@ -0,0 +1,24 @@
+
+
+public void Program()
+{
+
+}
+
+public void Save()
+{
+
+}
+
+
+public void Main(string argument, UpdateType updateSource)
+{
+    if((updateSource & UpdateType.Update100)==0)
+    {
+
+    }
+    else
+    {
+
+    }
+}
